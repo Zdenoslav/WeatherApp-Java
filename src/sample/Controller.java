5a888890ad391030135b7a7042d399451e06d8d6
ZDENKO MUCINA <MucinaZ@cardiff.ac.uk>
2020-03-29 20:16:55 +0100

    adding all files and starting properly

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+import java.awt.*;
+
 public class Controller {
+
+    @FXML
+    private TextField txtInput;
+
+    @FXML
+    private Button OkBtn;
+
+    @FXML
+    private TextArea txtOutput;
+
+    @FXML
+    private void printOutput() {
+        txtOutput.setText().getText();
+    }
 }
